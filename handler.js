7f3f26810088684b9398d124a14e3a838b3048e4
eulee0 <ec2-user@ip-172-31-24-50.us-west-2.compute.internal>
2017-02-15 14:42:53 +0000

    fixed some more

--- handler.js
+++ handler.js
@@ -1,103 +1,109 @@
+'use strict';
+
 var request = require('request');
-var parseString = require('xml2js').parseString;
 var AWS = require('aws-sdk');
 
 AWS.config.update({
-  region: "us-west-2"
+    region: "us-west-2"
 });
 
-'use strict';
+var docClient = new AWS.DynamoDB.DocumentClient({
+    region: "us-west-2"
+});
+var table = "BroncoBus";
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Updated Successfully!',
-      input: event,
-    }),
-  };
+module.exports.fetch = (event, context, callback) => {
+    // fetchBusTimes(event.pathParameters.name, callback);
+    const response = {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*"
+        },
+        body: JSON.stringify({
+            message: "Bus time has been fetched"
+        }),
+    };
 
-  callback(null, response);
+    fetchBusTimes();
+    callback(null, response);
+}
+
+module.exports.scan = (event, context, callback) => {
+    scanBusTimes(callback);
 };
-
-module.exports.queryBusTime = (event, context, callback) => {
-  queryBusTimes(event.pathParameters.name, callback);
-};
-
-var docClient = new AWS.DynamoDB.DocumentClient();
-var table = "BroncoBusAPI";
 
 function fetchBusTimes() {
-  request ('https://rqato4w151.execute-api.us-west-1.amazonaws.com/dev/info', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(body);
-      parseString(body, function (err, result) {
-        //console.dir (result.rss.channel[0].item);
-        var items = result.rss.channel[0].item;
-        for (var i = 0; i < items.length; i++) {
-          console.log (items[i].title[0], items[i].description[0]);
-          putItem(items[i].title[0], items[i].description[0]);
+    request('https://rqato4w151.execute-api.us-west-1.amazonaws.com/dev/info', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body);
+            var items = JSON.parse(body);
+            for (var i = 0; i < items.length; ++i) {
+                console.log(items[i]);
+                putItem(items[i].id, items[i].logo, items[i].lat, items[i].lng, items[i].route);
+            }
+        } else {
+            console.error("Thing don't work");
         }
-      });
-    } else {
-      console.log("Unable to read given API");
-    }
-  })
+    });
 }
 
-function putItem (id, logo, lat, lng, route) {
-  var params = {
-    TableName:table,
-    Item:{
-      "BusID": id,
-      "logo": logo,
-      "lat": lat,
-      "lng": lng,
-      "route": route,
-      "Timestamp": Date.now()
-    }
-  };
+function putItem(id, logo, lat, lng, route) {
+    var params = {
+        TableName: table,
+        Item: {
+            "id": id,
+            "logo": logo,
+            "lat": lat,
+            "lng": lng,
+            "route": route,
+            "timestamp": Date.now(),
+        }
+    };
 
-  console.log ("Adding new bus...");
-  docClient.put(params, function(err, data) {
-    if (err) {
-      console.error ("Unable to add bus. Error JSON:", JSON.stringify(err, null, 2));
-    } else {
-      console.log ("Added bus:", JSON.stringify(data, null, 2));
-    }
-  });
+    console.log("Adding new bus...");
+    docClient.put(params, function(err, data) {
+        if (err) {
+            console.error("Unable to add bus. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Added bus:", JSON.stringify(data, null, 2));
+        }
+    });
 }
 
-function queryBusTime(bus, res) {
-  var params = {
-      TableName : table,
-      KeyConditionExpression: "#key = :inputName",
-      ExpressionAttributeNames:{
-          "#key": "bus"
-      },
-      ExpressionAttributeValues: {
-          ":inputName":bus
-      }
-  };
+function scanBusTimes(callback) {
+    var params = {
+        TableName: table,
+    };
 
-docClient.query(params, function(err, data) {
-  if (err) {
-    console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
-      if (callback) {
-        const responseErr = {
-          statusCode: 500,
-          body: JSON.stringify({'err' : err}),
-        };
-        callback(null, responseErr);
-      }
-      console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
-          if (callback) {
-            const responseErr = {
-              statusCode: 500,
-              body: JSON.stringify({'err' : err}),
-            };
-            callback(null, responseErr);
-          }
+    docClient.scan(params, function(err, data) {
+        if (err) {
+            console.error("Unable to scan. Error:", JSON.stringify(err, null, 2));
+            if (callback) {
+                const responseErr = {
+                    statusCode: 500,
+                    headers: {
+                        "Access-Control-Allow-Origin": "*"
+                    },
+                    body: JSON.stringify({
+                        'err': err
+                    }),
+                };
+                callback(null, responseErr);
+            }
+        } else {
+            data.Items.forEach(function(item) {
+                console.log(item);
+            });
+            if (callback) {
+                const responseOk = {
+                    statusCode: 200,
+                    headers: {
+                        "Access-Control-Allow-Origin": "*"
+                    },
+                    body: JSON.stringify(data.Items)
+                };
+                callback(null, responseOk);
+            }
         }
-      });
-    }
+    });
+}
